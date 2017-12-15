b5f9e1a9967ba05c96cd054849c370fc5a394e5b
engelspencer <dasparkster101@gmail.com>
2017-12-15 11:28:40 -0600

    commented on some things, added some others

--- classpet.py
+++ classpet.py
@@ -1,13 +1,16 @@
 from sense_hat import SenseHat
 import time
 
+#animate function
 def animate(sense, slides, sleeptime):
 	for slide in slides:
 		sense.set_pixels(slide)
 		time.sleep(sleeptime)
 
+#instantiation of SenseHat object
 sense = SenseHat()
 
+#color rgb values used in animations
 h = (216, 157, 28) #horn
 l = (100, 100, 115) #light metal
 d = (70, 70, 85) #dark metal
@@ -17,7 +20,8 @@ o = (136, 86, 35) #shoe leather
 e = (0, 0, 0) #eyes and mouth
 b = (180, 180, 180) #background
 
-knight = [
+#idle avatar
+dovahkiin = [
 b, h, l, s, e, d, e, s,
 h, h, l, s, s, d, s, s,
 d, b, l, s, e, e, e, s,
@@ -28,7 +32,8 @@ d, a, s, l, l, d, d, l,
 b, b, b, o, a, o, a, b
 ]
 
-runningKnight = [[
+#entertain animation
+chargingDovahkiin = [[
 b, h, l, s, e, d, e, s,
 h, h, l, s, s, d, s, s,
 d, b, l, s, e, e, e, s,
@@ -66,16 +71,21 @@ l, d, s, l, l, d, d, l,
 d, a, b, o, a, b, o, a
 ]]
 
+#feeding animation
+feastingDovahkiin = [[
+
+]]
+
 while True:
-	sense.set_pixels(knight)
+	sense.set_pixels(dovahkiin)
 	x,y,z = sense.get_accelerometer_raw().values()
 	orientation = sense.get_orientation()
 
 	if (x > 1 or y > 1 or z > 1):
-		animate (sense, runningKnight, .1)
-		animate (sense, runningKnight, .1)
-		animate (sense, runningKnight, .1)
-		animate (sense, runningKnight, .1)
+		animate (sense, chargingDovahkiin, .1)
+		animate (sense, chargingDovahkiin, .1)
+		animate (sense, chargingDovahkiin, .1)
+		animate (sense, chargingDovahkiin, .1)
 	
 	if ((orientation['roll'] > 160 and orientation['roll'] < 180) or (orientation['yaw'] > 200 and orientation['yaw'] < 220)):
-		animate (sense, mead, .5)
+		animate (sense, feastingDovahkiin, .5)
